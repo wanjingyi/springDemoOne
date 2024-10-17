af921c35bc596e58892e33ccf55e45b9adecae12
wangjingyi <wangjingyi@tfzq.com>
2024-10-17 16:19:33 +0800

    2-2 init springboot初始化

---
+++ src/main/java/com/example/demospringone/DemoSpringOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringone/DemoSpringOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
